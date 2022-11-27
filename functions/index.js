d658d1283d4e4f21f9dbc1601edee89c857ea2b0
Kapakahi <rubbishman@gmail.com>
2022-11-27 15:30:40 +0900

    store form data in firebase

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,23 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
+
+// http callable function to add a user
+exports.addUser = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "You must be authenticated to add a user"
+    );
+  }
+  return admin.firestore().collection("users").add({
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    age: data.age,
+    address: {street: data.street,
+      city: data.city,
+      state: data.state,
+      zipCode: data.zipCode},
+  });
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,39 @@
-const requestModal = document.querySelector(".new-user");
-const requestLink = document.querySelector(".add-user");
+const newUserModal = document.querySelector(".new-user");
+const addUserLink = document.querySelector(".add-user");
+const newUserForm = document.querySelector(".new-user form");
 
 // open add user modal
-requestLink.addEventListener("click", () => {
-  requestModal.classList.add("open");
+addUserLink.addEventListener("click", () => {
+  newUserModal.classList.add("open");
 });
 
 // close add user modal
-requestModal.addEventListener("click", (e) => {
+newUserModal.addEventListener("click", (e) => {
   if (e.target.classList.contains("new-user")) {
-    requestModal.classList.remove("open");
+    newUserModal.classList.remove("open");
   }
+});
+
+// add new user
+newUserForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const addUser = firebase.functions().httpsCallable("addUser");
+  addUser({
+    firstName: newUserForm.firstName.value,
+    lastName: newUserForm.lastName.value,
+    email: newUserForm.email.value,
+    age: newUserForm.age.value,
+    street: newUserForm.street.value,
+    city: newUserForm.city.value,
+    state: newUserForm.state.value,
+    zipCode: newUserForm.zipCode.value,
+  })
+  .then(()=> {
+    newUserForm.reset();
+    newUserModal.classList.remove("open");
+    newUserForm.querySelector(".error").textContent = "";
+  })
+  .catch(error => {
+    newUserForm.querySelector(".error").textContent = error.message;
+  });
 });
